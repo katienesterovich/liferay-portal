2351c8ce5e4b1375513e8ecf6e261ea823a54c56
katienesterovich <ekaterina.nesterovich@liferay.com>
2020-11-24 10:42:24 -0600

    COMMERCE-5281 Create Avalara dispatch

---
+++ modules/dxp/apps/commerce-avalara/dispatch-avalara-web/src/main/java/com/liferay/dispatch/avalara/web/internal/executor/AvalaraDispatchTaskExecutor.java
@@ -0,0 +1,77 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ *
+ *
+ *
+ */
+
+package com.liferay.dispatch.avalara.web.internal.executor;
+
+import com.liferay.commerce.avalara.connector.helper.CommerceAvalaraConnectorHelper;
+import com.liferay.dispatch.executor.BaseDispatchTaskExecutor;
+import com.liferay.dispatch.executor.DispatchTaskExecutor;
+import com.liferay.dispatch.executor.DispatchTaskExecutorOutput;
+import com.liferay.dispatch.model.DispatchTrigger;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.util.UnicodeProperties;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Katie Nesterovich
+ */
+@Component(
+	immediate = true,
+	property = "dispatch.task.executor.type=" + AvalaraDispatchTaskExecutor.DISPATCH_TASK_EXECUTOR_TYPE_AVALARA,
+	service = DispatchTaskExecutor.class
+)
+public class AvalaraDispatchTaskExecutor extends BaseDispatchTaskExecutor {
+
+	public static final String DISPATCH_TASK_EXECUTOR_TYPE_AVALARA = "avalara";
+
+	@Override
+	public void doExecute(
+			DispatchTrigger dispatchTrigger,
+			DispatchTaskExecutorOutput dispatchTaskExecutorOutput)
+		throws PortalException {
+
+		UnicodeProperties unicodeProperties =
+			dispatchTrigger.getTaskSettingsUnicodeProperties();
+
+		String groupId = GetterUtil.getString(
+			unicodeProperties.getProperty(_GROUP_ID));
+
+		try {
+			_commerceAvalaraConnectorHelper.updateByAddressEntries(
+				Long.parseLong(groupId));
+		}
+		catch (Exception exception) {
+			_log.error("Unable to update By Address entries", exception);
+		}
+	}
+
+	@Override
+	public String getName() {
+		return DISPATCH_TASK_EXECUTOR_TYPE_AVALARA;
+	}
+
+	private static final String _GROUP_ID = "groupId";
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		AvalaraDispatchTaskExecutor.class);
+
+	@Reference
+	private CommerceAvalaraConnectorHelper _commerceAvalaraConnectorHelper;
+
+}
