3b9c4b604fe9c9b6eb2f06f963b8fcde4fdb8535
katienesterovich <ekaterina.nesterovich@liferay.com>
2020-12-22 18:38:04 -0600

    COMMERCE-5371 Update avalara channel configuration UI

--- modules/dxp/apps/commerce-avalara/commerce-avalara-tax-engine-fixed-web/src/main/java/com/liferay/commerce/avalara/tax/engine/fixed/web/internal/frontend/taglib/servlet/taglib/CommerceTaxMethodAvalaraScreenNavigationCategory.java
+++ modules/dxp/apps/commerce-avalara/commerce-avalara-tax-engine-fixed-web/src/main/java/com/liferay/commerce/avalara/tax/engine/fixed/web/internal/frontend/taglib/servlet/taglib/CommerceTaxMethodAvalaraScreenNavigationCategory.java
@@ -14,7 +14,10 @@
 
 package com.liferay.commerce.avalara.tax.engine.fixed.web.internal.frontend.taglib.servlet.taglib;
 
+import com.liferay.commerce.avalara.connector.CommerceAvalaraConnector;
+import com.liferay.commerce.avalara.connector.configuration.CommerceAvalaraConnectorChannelConfiguration;
 import com.liferay.commerce.avalara.connector.configuration.CommerceAvalaraConnectorConfiguration;
+import com.liferay.commerce.avalara.connector.constants.CommerceAvalaraConstants;
 import com.liferay.commerce.constants.CommerceTaxScreenNavigationConstants;
 import com.liferay.commerce.tax.model.CommerceTaxMethod;
 import com.liferay.commerce.tax.service.CommerceTaxMethodService;
@@ -24,6 +27,7 @@ import com.liferay.frontend.taglib.servlet.taglib.util.JSPRenderer;
 import com.liferay.portal.kernel.language.LanguageUtil;
 import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.kernel.module.configuration.ConfigurationProvider;
+import com.liferay.portal.kernel.settings.CompanyServiceSettingsLocator;
 import com.liferay.portal.kernel.settings.GroupServiceSettingsLocator;
 import com.liferay.portal.kernel.settings.ParameterMapSettingsLocator;
 import com.liferay.portal.kernel.util.ParamUtil;
@@ -31,7 +35,9 @@ import com.liferay.portal.kernel.util.ResourceBundleUtil;
 
 import java.io.IOException;
 
+import java.util.HashMap;
 import java.util.Locale;
+import java.util.Map;
 import java.util.ResourceBundle;
 
 import javax.servlet.ServletContext;
@@ -92,7 +98,7 @@ public class CommerceTaxMethodAvalaraScreenNavigationCategory
 
 		String engineKey = commerceTaxMethod.getEngineKey();
 
-		if (engineKey.equals("avalara")) {
+		if (engineKey.equals(CommerceAvalaraConstants.KEY)) {
 			return true;
 		}
 
@@ -113,20 +119,33 @@ public class CommerceTaxMethodAvalaraScreenNavigationCategory
 				_commerceTaxMethodService.getCommerceTaxMethod(
 					commerceTaxMethodId);
 
-			CommerceAvalaraConnectorConfiguration
-				commerceAvalaraConnectorConfiguration =
+			CommerceAvalaraConnectorChannelConfiguration
+				commerceAvalaraConnectorChannelConfiguration =
 					_configurationProvider.getConfiguration(
-						CommerceAvalaraConnectorConfiguration.class,
+						CommerceAvalaraConnectorChannelConfiguration.class,
 						new ParameterMapSettingsLocator(
 							httpServletRequest.getParameterMap(),
 							new GroupServiceSettingsLocator(
 								commerceTaxMethod.getGroupId(),
-								CommerceAvalaraConnectorConfiguration.class.
-									getName())));
+								CommerceAvalaraConnectorChannelConfiguration.
+									class.getName())));
 
 			httpServletRequest.setAttribute(
-				CommerceAvalaraConnectorConfiguration.class.getName(),
-				commerceAvalaraConnectorConfiguration);
+				CommerceAvalaraConnectorChannelConfiguration.class.getName(),
+				commerceAvalaraConnectorChannelConfiguration);
+
+			if (_verifyConnection(
+					httpServletRequest, commerceTaxMethod.getCompanyId())) {
+
+				httpServletRequest.setAttribute(
+					"connectionEstablished", Boolean.TRUE);
+
+				_setCompanies(httpServletRequest);
+			}
+			else {
+				httpServletRequest.setAttribute(
+					"connectionEstablished", Boolean.FALSE);
+			}
 		}
 		catch (Exception exception) {
 			throw new IOException(exception);
@@ -136,6 +155,48 @@ public class CommerceTaxMethodAvalaraScreenNavigationCategory
 			_servletContext, httpServletRequest, httpServletResponse,
 			"/avalara_settings.jsp");
 	}
+
+	private void _setCompanies(HttpServletRequest httpServletRequest) {
+		Map<String, String> companies = new HashMap<>();
+
+		try {
+			companies = new HashMap<>();
+		}
+		catch (Exception exception) {
+		}
+
+		httpServletRequest.setAttribute("avalaraCompanies", companies);
+	}
+
+	private boolean _verifyConnection(
+		HttpServletRequest httpServletRequest, long companyId) {
+
+		try {
+			CommerceAvalaraConnectorConfiguration
+				commerceAvalaraConnectorConfiguration =
+					_configurationProvider.getConfiguration(
+						CommerceAvalaraConnectorConfiguration.class,
+						new ParameterMapSettingsLocator(
+							httpServletRequest.getParameterMap(),
+							new CompanyServiceSettingsLocator(
+								companyId,
+								CommerceAvalaraConnectorConfiguration.class.
+									getName())));
+
+			_commerceAvalaraConnector.verifyConnection(
+				commerceAvalaraConnectorConfiguration.accountNumber(),
+				commerceAvalaraConnectorConfiguration.licenseKey(),
+				commerceAvalaraConnectorConfiguration.serviceURL());
+
+			return true;
+		}
+		catch (Exception exception) {
+			return false;
+		}
+	}
+
+	@Reference
+	private CommerceAvalaraConnector _commerceAvalaraConnector;
 
 	@Reference
 	private CommerceTaxMethodService _commerceTaxMethodService;
