1925e9c595e99e9f7e5169224bb0140969f13075
katienesterovich <ekaterina.nesterovich@liferay.com>
2021-01-22 12:43:04 -0600

    COMMERCE-5513 Create CommerceAddressValidationEngine API

---
+++ modules/dxp/apps/commerce/commerce-address-validation-engine-api/src/main/java/com/liferay/commerce/address/validation/engine/CommerceAddressValidationEngine.java
@@ -0,0 +1,32 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ *
+ *
+ *
+ */
+
+package com.liferay.commerce.address.validation.engine;
+
+import com.liferay.commerce.model.CommerceAddress;
+
+import java.util.List;
+
+/**
+ * @author Katie Nesterovich
+ */
+public interface CommerceAddressValidationEngine {
+
+	public String getAddressValidationEngineName();
+
+	public boolean isCommerceAddressValid(CommerceAddress commerceAddress);
+
+	public List<CommerceAddress> validate(CommerceAddress commerceAddress);
+
+}
